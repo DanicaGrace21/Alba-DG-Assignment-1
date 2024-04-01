bf1465a8197b02ef94987f43fff7c0fc7050dc2c
Danica <165607381+DanicaGrace21@users.noreply.github.com>
2024-04-01 11:32:47 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Alba_DG_Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "Admin";
+
+            string password = "Admin1234";
+
+            int Tries = 3;
+
+            while (Tries >= 1)
+            {
+                Console.Write("Username : ");
+                string A = Console.ReadLine();
+
+                Console.Write("Password : ");
+                string B = Console.ReadLine();
+
+                if (A == username && B == password) break;
+                else
+                {
+                    Tries--;
+                    Console.WriteLine("Wrong Credentials");
+                    Console.WriteLine();
+
+                    if (Tries >= 1)
+                    {
+                        Console.WriteLine(Tries + " more attempts");
+                        Console.WriteLine();
+                    }
+                }s
+            }
+            if (Tries >= 1)
+            {
+                Console.WriteLine("Login Successfully");
+            }
+            else
+            {
+                Console.WriteLine("maximum number of attempts made");
+            }
+        }
+    }
+}
